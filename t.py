5f8e1ebc7603dc5d79fa4141d4748c5fa2f2393b
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2018-09-09 23:09:58 +0900

    update

--- t.py
+++ t.py
@@ -1,51 +1,60 @@
 # -*- coding: utf-8 -*-
 import json
 import os
+import csv
 import re
-import unicodecsv as csv
+from collections import defaultdict
+import datetime
 import codecs
 
-def wakati(s):
-    import MeCab
-    tagger = MeCab.Tagger("-Owakati")
-    words = tagger.parse(s.encode('utf-8')).strip().decode("utf-8").split()
-    return words
-
-INFILE = "tweets_20170403.csv"
-TITLE_PREFIX = ""
-KEYWORDS = u"KJ法 生産性 川喜田 外山".split()
 pages = []
-fo = file("text.txt", "w")
-reader = csv.reader(file(INFILE), encoding="utf-8")
+def add_page(title, lines):
+    page = dict(
+        title=title, 
+        lines=[title] + lines)
+    pages.append(page)    
+
+ymd_to_tweet = defaultdict(list)
+ym_to_ymd = defaultdict(set)
+y_to_ym = defaultdict(set)
+
+INFILE = "tweets.csv"
+pages = []
+reader = csv.reader(open(INFILE, encoding="utf-8"))
+
 for row in reader:
     tid = row[0]
-    datetime = row[3]
+    s_datetime = row[3]
     text = row[5]
-    lines = []
-    #title = u"{prefix}{}".format(tid, prefix=TITLE_PREFIX)
-    title = u"{prefix}{}".format(text, prefix=TITLE_PREFIX)
-    lines.append(title)
-    lines.append(text)
+    if s_datetime == "timestamp": continue  # first line
+    
+    dt = datetime.datetime.strptime(s_datetime, "%Y-%m-%d %H:%M:%S %z")
+    ymd = dt.strftime("%Y-%m-%d")
+    ym = dt.strftime("%Y-%m")
+    y = dt.strftime("%Y")
+    url = "https://twitter.com/nishio/status/{}".format(tid)
+    link_to_tweet = "[T {}]".format(url)
 
-    lines.append("https://twitter.com/nishio/status/{}".format(tid))
-    lines.append(datetime)
-    date = datetime.split()[0]
-    ks = [date]
-    for k in KEYWORDS:
-        if k in text:
-            ks.append(k)
-    lines.append(" ".join(u"[%s]" % k for k in ks))
-    page = dict(title=title, lines=lines)
-    pages.append(page)
+    ymd_to_tweet[ymd].append(" {} {}".format(text, link_to_tweet))
+    ym_to_ymd[ym].add(ymd)
+    y_to_ym[y].add(ym)
 
-    text = re.sub(u"@\w+", u" ", text)
-    text = re.sub(u"http[\w:./]+", u" ", text)
-    text = text.replace("_", "UNDERSCORE")
-    text = text.replace(" ", "_")
-    #text = u" ".join(text)
-    text = u" ".join(wakati(text))
 
-    fo.write(text.encode("utf8"))
-    fo.write("\n")
+for y in y_to_ym:
+    add_page(
+        title=y, 
+        lines=["[%s]" % ym for ym in sorted(y_to_ym[y])])
+
+for ym in ym_to_ymd:
+    add_page(
+        title=ym,
+        lines=["[%s]" % ymd for ymd in sorted(ym_to_ymd[ym])])
+
+for ymd in sorted(ymd_to_tweet):
+    add_page(
+        title=ymd,
+        lines=list(reversed(ymd_to_tweet[ymd])))
 
 json.dump(dict(pages=pages), codecs.open('to_scrapbox.json', 'w', encoding="utf-8"), ensure_ascii=False, indent=2)
+
+
